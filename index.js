9c527e7449da51e417a4b79788e900bc2ea638cd
aymenby <aymenby30@gmail.com>
2020-12-29 12:39:29 +0100

    btns

--- index.js
+++ index.js
@@ -1,40 +1,52 @@
-var add = document.querySelector(".fa-plus-square");
-var minus = document.querySelector(".fa-minus-square");
-var container = document.querySelector(".container");
-var qte = document.querySelector('.qte');
-var deletebtn = document.querySelector('.deletebtn');
-var card = document.querySelector('.card');
+var add = document.querySelectorAll(".fa-plus-square");
+var minus = document.querySelectorAll(".fa-minus-square");
+var container = document.querySelectorAll(".container");
+var qte = document.querySelectorAll('.qte');
+var deletebtn = document.querySelectorAll('.deletebtn');
+var card = document.querySelectorAll('.card');
 var total = document.querySelector('.total');
 var price = document.querySelector('.price');
-var heart = document.querySelector('.fa-heart');
-var i = 1;
+var heart = document.querySelectorAll('.fa-heart');
 
-add.addEventListener("click", () => {
-    
-    if (qte.value >= 0){
-        qte.value = parseInt(qte.value)+1
-    }
-    
-    total.innerHTML = (qte.value)*price.innerHTML;
-})
-minus.addEventListener("click",()=>{
-    
-    if(qte.value<0){
-        qte.value = 0;
-    }else if(qte.value>0){
-        qte.value = parseInt(qte.value)-1
-    }
-    
-    total.innerHTML = (qte.value)*price.innerHTML;
 
-})
-deletebtn.addEventListener("click",()=>{
-    card.remove();
-})
-heart.addEventListener("click",()=>{
-    heart.classList.toggle("mystyle");
+/*function plus*/
+
+Array.from(add).map(add => {
+    add.addEventListener("click", () => {
+        
+        if (qte.value >= 0){
+            qte.value = parseInt(qte.value)+1
+        }
+        
+        total.innerHTML = (qte.value)*(price.innerHTML);
+    })
 })
 
+// minus.addEventListener("click",()=>{
+    
+//     if(qte.value<0){
+//         qte.value = 0;
+//     }else if(qte.value>0){
+//         qte.value = parseInt(qte.value)-1
+//     }
+    
+//     total.innerHTML = (qte.value)*price.innerHTML;
+
+// })
 
 
+// heart.addEventListener("click",()=>{
+//     heart.classList.toggle("mystyle");
 
+//     // if( heart.classList.toggle === "far fa-heart mystyle"){
+//     //     var row = document.createElement("span");
+//     //     row.innerHTML = 'Item added'
+//     // }
+// })
+
+
+Array.from(deletebtn).map(deletebtn => {
+    deletebtn.addEventListener("click",()=>{
+    deletebtn.parentElement.parentElement.remove();
+})
+})
